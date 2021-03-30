5b5961612b4bc27b75705f3b8a4110d2bfe2e7b9
Joel <joel.d.hart1@gmail.com>
2021-03-30 01:01:06 +0000

    added 2 queries

---
+++ 2_queries_joins/1_student_total_assignment_duration.sql
@@ -0,0 +1,13 @@
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
+
+
+
+-- SELECT students.name, cohorts.name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
+-- FROM students
+-- JOIN cohorts ON cohort_id = cohorts.id
+-- WHERE cohorts.start_date != students.start_date
+-- ORDER BY cohort_start_date;
