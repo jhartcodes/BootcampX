8037a760ef580509e61ac39e607307754f419f35
Joel <joel.d.hart1@gmail.com>
2021-04-01 02:03:21 +0000

    Setup the Javascript project

--- 4_queries/name_Ofteacher_assisted.sql
+++ 4_queries/name_Ofteacher_assisted.sql
@@ -7,11 +7,3 @@ WHERE cohorts.name = 'JUL02'
 ORDER BY teacher;
 
 
-
-
-Get the name of all teachers that performed an assistance request during a cohort.
-
-Select the instructor's name and the cohort's name.
-Don't repeat the instructor's name in the results list.
-Order by the instructor's name.
-This query needs to select data for a cohort with a specific name, use 'JUL02' for the cohort's name here.
